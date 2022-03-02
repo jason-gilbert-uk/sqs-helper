11e588a11b8ff9f3977c1fc4d32d3784f436c6a1
Jason Gilbert <jasongilbert@jasons-mac-mini.home>
2022-03-02 14:22:44 +0000

    latest

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
 # sqs-helper
+ A selection of helper functions when working with SQS
+
+## writeObjectToSQS(queueURL,object)
+Writes a JSON object to the SQS queue. Returns the id of the message written. Throws exception in any unexpected situation.
+```
+const {writeObjectToSQS,createSQS} = require('@jasongilbertuk/sqs-helper)
+try {
+    const myQueueURL = await createSQS('mySQSQueue')
+    const myObject = {name: 'example object'}
+    const msgId = await writeObjectToSQS(myQueueURL,myObject);
+} catch (err) {
+    //Your error handling logic
+    console.log(err);
+}
+```
+
+## readObjectFromSQS(queueURL)
+Reads JSON objects from the SQS queue. Returns stucture including all the messages read.
+```
+const {writeObjectToSQS,readObjectsFromSQS,createSQS} = require('@jasongilbertuk/sqs-helper)
+try {
+    const myQueueURL = await createSQS('mySQSQueue')
+    const myObject = {name: 'example object'}
+    const msgId = await writeObjectToSQS(myQueueURL,myObject);
+    const result = await readObjectFromSQS(myQueueURL);
+    console.log(result.Messages);
+} catch (err) {
+    //Your error handling logic
+    console.log(err);
+}
+```
+## createSQS(queueName)
+Creates an SQS queue. Returns the URL of the queue. Throws exception on unexpected errors.
+```
+const {createSQS} = require('@jasongilbertuk/sqs-helper)
+try {
+    const myQueueURL = await createSQS('mySQSQueue')
+} catch (err) {
+    //Your error handling logic
+    console.log(err);
+}
+```
+## createSQSIfDoesntExist(queueName)
+Creates a named sqs queue if it doesn't already exist. Either way, returns the name of the queues url.
+```
+const {createSQSIfDoesntExist} = require('@jasongilbertuk/sqs-helper)
+try {
+    const myQueueURL = await createSQSIfDoesntExist('mySQSQueue')
+} catch (err) {
+    //Your error handling logic
+    console.log(err);
+}
+```
+## deleteSQS(queueURL)
+Deletes the sqs queue. Throws exception on unexpected errors.
+```
+const {deleteSQS} = require('@jasongilbertuk/sqs-helper)
+try {
+    const result = await deleteSQS(myQueueUrl);
+} catch (err) {
+    //Your error handling logic
+    console.log(err);
+}
+```
---
+++ index.js
@@ -0,0 +1,151 @@
+const AWS = require('aws-sdk')
+AWS.config.update({region:'eu-west-1'});
+const SQS = new AWS.SQS({apiVersion:'2012-11-05'});
+
+//----------------------------------------------------------------------------
+// function: createSQS(queueName)
+// Creates an SQS queue. Returns the URL of the queue.
+// Throws exception on unexpected errors.
+//----------------------------------------------------------------------------
+async function createSQS(queueName) {
+    var params = {
+        QueueName: queueName, /* required */
+        Attributes: {}
+      };
+      try {
+          var result = await SQS.createQueue(params).promise();
+          return result.QueueUrl;
+      } catch (err) {
+        console.log("error in sqs-helper.createSQS. ",err); // an error occurred
+        throw err;
+      }
+}
+
+//----------------------------------------------------------------------------
+// function: checkSQSExists(queueName)
+// Returns url of queue if the named queue exists, null otherwise.
+// Throws exception on unexpected errors.
+//----------------------------------------------------------------------------
+async function checkSQSExists(queueName) {
+    var result = null;
+
+    var params = {
+        QueueName: queueName
+    }
+
+    try {
+        var returnValue= await SQS.getQueueUrl(params).promise();
+        return returnValue.QueueUrl;
+
+    } catch (err) {
+        if (err.code === 'AWS.SimpleQueueService.NonExistentQueue') {
+            result = null
+            return result;
+        } else {
+            console.log('error in sqs-helper.checkSQSExists : ',err);
+            throw err;
+        }
+    }
+
+}
+
+//----------------------------------------------------------------------------
+// function: createSQSIfDoesntExist(queueName)
+// creates a named sqs queue if it doesn't already exist.
+// returns the name of the queues url.
+//----------------------------------------------------------------------------
+async function createSQSIfDoesntExist(queueName) {
+    var result = await checkSQSExists(queueName);
+    if ( result ) {
+        return result;
+    } else {
+        return await createSQS(queueName);
+    }
+}
+
+//----------------------------------------------------------------------------
+// function: writeObjectToSQS(queueURL,object)
+// writes a JSON object to the SQS queue
+// returns the id of the message written. Throws error in any unexpected
+// situation.
+//----------------------------------------------------------------------------
+async function writeObjectToSQS(queueURL,object) {
+    var params = {
+       DelaySeconds: 10,
+       MessageAttributes: {},
+       MessageBody: JSON.stringify(object),
+       QueueUrl: queueURL
+     };
+     
+     try {
+         var result = await SQS.sendMessage(params).promise();
+         return result.MessageId;
+    
+     } catch (err) {
+         console.log('Error encountered in sqs-helper.writeObjectToSQS : ',err);
+         throw err;
+     }
+}
+
+//----------------------------------------------------------------------------
+// function: readObjectFromSQS(queueURL)
+// Reads JSON objects from the SQS queue
+// returns stucture including all the messages read.
+//----------------------------------------------------------------------------
+async function readObjectFromSQS(queueUrl) {
+    var params = {
+        AttributeNames: [
+           "SentTimestamp"
+        ],
+        MaxNumberOfMessages: 10,
+        MessageAttributeNames: [
+           "All"
+        ],
+        QueueUrl: queueUrl,
+        VisibilityTimeout: 20,
+        WaitTimeSeconds: 5
+       };
+       
+       try {
+           var result = await SQS.receiveMessage(params).promise();
+           if (result.Messages) {
+               result.Messages.map(async (msg)=>{
+                    var deleteParams = {
+                        QueueUrl: queueName,
+                        ReceiptHandle: msg.ReceiptHandle
+                    };
+                    var result2 = await SQS.deleteMessage(deleteParams).promise();
+               })
+               return result;
+            }
+       } catch (err) {
+            console.log("Error encountered in sqs-helper.readObjectFromSQS : ", err); 
+            throw err; 
+       }
+}
+
+//----------------------------------------------------------------------------
+// function: deleteSQS(queueURL)
+// deletes the sqs queue
+// returns stucture including all the messages read.
+//----------------------------------------------------------------------------
+async function deleteSQS(queueUrl) {
+
+    var params = {
+            QueueUrl: queueURL
+        };
+    try {
+        var result = await SQS.deleteQueue(params).promise();
+        return result;
+    } catch (err) {
+        console.log('error encountered in sqs-helper.deleteSQS : ',err);
+        throw err;
+    }
+}
+
+
+exports.writeObjectToSQS = writeObjectToSQS;
+exports.readObjectFromSQS = readObjectFromSQS;
+exports.createSQS = createSQS;
+exports.createSQSIfDoesntExist = createSQSIfDoesntExist;
+exports.deleteSQS = deleteSQS;
